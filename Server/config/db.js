27af8d609decbe98e87253ca0c259b5fce04be68
krishna-85 <vaibhavchauhan658@gmail.com>
2025-03-22 16:24:24 +0530

    mongo uri change

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-
 // Function to connect to the mongodb database
 const connectDB = async ()=>{
     mongoose.connection.on('connected',()=> console.log('Database connected'))
