73b16016bab3a66bbb996855eac886d8d1f73c92
krishna-85 <vaibhavchauhan658@gmail.com>
2025-03-23 01:04:46 +0530

    remove unused server files and configurations

--- Server/routes/clerkWebhookRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { clerkWebhooks } from '../controllers/webhooks.js';
-
-const router = express.Router();
-
-// Webhook route
-router.post('/clerk', clerkWebhooks);
-
-export default router;
